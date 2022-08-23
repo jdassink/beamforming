c0c0c92fd234734a17926a0564ed7332ad16a735
Jelle Assink <assink@knmi.nl>
2022-08-23 23:04:32 +0200

    Python libs

---
+++ python/waveforms/metadata.py
@@ -0,0 +1,281 @@
+"""
+Metadata interface functions
+
+.. module:: metadata
+
+:author:
+    Jelle Assink (jelle.assink@knmi.nl)
+
+:copyright:
+    2021, Jelle Assink
+
+:license:
+    This code is distributed under the terms of the
+    GNU General Public License, Version 3
+    (https://www.gnu.org/licenses/gpl-3.0.en.html)
+"""
+from obspy import read_inventory
+from obspy.clients.filesystem.sds import Client as sds_client
+from obspy.clients.fdsn import Client as fdsn_client
+from obspy.clients.fdsn import RoutingClient
+from obspy.core.inventory.inventory import Inventory
+from obspy.signal.util import util_geo_km
+from obspy.geodetics.base import gps2dist_azimuth
+
+from pandas import DataFrame
+from lxml import etree as ET
+import numpy as np
+
+class param_object(object):
+    pass
+
+def compute_aperture(coords):
+    aperture = 0
+
+    n_sta = len(coords)
+    for i in range(0, n_sta):
+        for j in range(i+1, n_sta):
+            (d,_,_) = gps2dist_azimuth(coords[i][0], coords[i][1],
+                                        coords[j][0], coords[j][1])
+            if d > aperture:
+                aperture = d
+    return aperture
+
+def compute_offsets(cha, ref):
+    (x, y) = util_geo_km(ref.longitude, ref.latitude,
+                         cha.longitude, cha.latitude)
+    return (x,y)
+
+def df_to_ascii(df, fid_out, print_offset=False, verbose=False):
+    """
+    Write out stationtable as formatted ASCII table
+    """
+    if verbose:
+        print ('Writing to %s' % fid_out)
+
+    with open(fid_out, 'w') as f:   
+        for _, r in df.iterrows():
+            seed_id = f'{r.network}.{r.station}.{r.location}.{r.channel}'
+            line  = f'{seed_id:>18s} '
+
+            if print_offset:
+                line += f'{r.deast:17.4f} {r.dnorth:17.4f} '
+            line += f'{r.elevation:17.4f} {r.edepth:17.4f} '
+            line += f'{r.latitude:9.6f} {r.longitude:9.6f} '
+            line += f'{r.starttime:8d} {r.endtime:8d} '
+            line += f'{r.gain:15.4e}\n'
+            f.write(line)
+    return
+
+def get_array_reference(inv, reference='first'): 
+    ref = param_object()
+
+    if reference == 'first':
+        ref.latitude = inv[0][0][0].latitude
+        ref.longitude = inv[0][0][0].longitude
+
+    elif reference == 'center':
+        data = []
+        for seed_id in inv.get_contents()['channels']:
+            data.append(inv.get_coordinates(seed_id))
+        data = DataFrame(data).drop_duplicates()
+        ref.latitude = data['latitude'].mean()
+        ref.longitude = data['longitude'].mean()
+    return ref
+
+def get_gain(inv, tr, starttime=None, endtime=None):
+    invs = inv.select(network=tr.stats.network,
+                        station=tr.stats.station,
+                        location=tr.stats.location,
+                        channel=tr.stats.channel,
+                        starttime=starttime,
+                        endtime=endtime)
+    return invs[0][0][0].response.instrument_sensitivity.value
+
+def get_metadata(data_source, network, station, location, channel,
+                 starttime, endtime, **kwargs):
+    """
+    source type options:
+
+    local - local stationxml file
+    fdsn - typical dataselect
+    routing - 'iris-federator' or 'eida-routing'   
+    """
+    invselect = locals()
+    del(invselect['data_source'])
+    del(invselect['kwargs'])
+    [(key, value)] = data_source.items()
+
+    if key == 'local':
+        inv = read_inventory(value, 'STATIONXML')
+        inv = inv.select(**invselect)
+
+    elif key == 'fdsn':
+        cl = fdsn_client(value)
+        inv = cl.get_stations(**invselect, **kwargs, level='response')
+
+    elif key == 'routing':
+        cl = RoutingClient(value)
+        inv = cl.get_stations(**invselect, **kwargs, level='response')
+
+    else:
+        print ('meta-data source should be "local" or "fdsn"')
+        inv = []
+    return inv
+
+def inv_to_coords(inv):
+    """
+    Convert inventory with items to Numpy array for array
+    response calculations
+    """
+    n_stations = len(inv.get_contents()['stations'])
+    coords = np.zeros((n_stations, 3))
+    i = 0
+
+    for i in range(0, n_stations):
+        coords[i,0] = inv[0][i].latitude
+        coords[i,1] = inv[0][i].longitude
+        coords[i,2] = inv[0][i].elevation
+
+    return coords
+
+def inv_to_df(inv, compute_offset=False, offset_reference='first'):
+    data = []
+
+    for net in inv:
+        for sta in net:
+            for cha in sta:
+                t0 = parse_utcdatetime(cha.start_date)
+                t1 = parse_utcdatetime(cha.end_date)
+                try:
+                    gain = cha.response.instrument_sensitivity.value
+                except:
+                    gain = -1
+                item = dict(network=net.code,
+                            station=sta.code,
+                            location=cha.location_code,
+                            channel=cha.code,
+                            latitude=cha.latitude,
+                            longitude=cha.longitude,
+                            elevation=cha.elevation,
+                            edepth=cha.depth,
+                            starttime=t0,
+                            endtime=t1,
+                            gain=gain)
+
+                if compute_offset:
+                    ref = get_array_reference(inv, offset_reference)
+                    (x, y) = compute_offsets(cha, ref)
+                    item['deast'] = x*1e3
+                    item['dnorth'] = y*1e3
+
+                data.append(item)
+    df = DataFrame(data).drop_duplicates()
+    return df
+
+def inv_to_dtk_xml(inv, array_name):
+    """
+    Function to convert StationXML to XML tree object that is compatible with DTK-GPMCC
+    """
+    ns ={ None: 'http://www.fdsn.org/xml/station/1',
+        'dtk': 'http://www.fdsn.org/xml/dtk/1'}
+
+    date_fmt = '%Y-%m-%dT%H:%M:%S'
+    root = ET.Element('FDSNStationXML', nsmap=ns,
+                    schemaVersion=u"1")
+
+    for net in inv:
+        network = ET.SubElement(root, 'Network', code=f"{net.code}")
+        nsta = ET.SubElement(network, 'TotalNumberStations')
+        nsta.text = f'{len(inv.get_contents()["networks"])}'
+        
+        ref_sta = net[0]
+        t0 = ref_sta.start_date.strftime(date_fmt)
+        try:
+            t1 = ref_sta.end_date.strftime(date_fmt)
+        except:
+            t1 = None
+        station = ET.SubElement(network, 'Station',
+                                code=f'{array_name}',
+                                startDate=f'{t0}',
+                                endDate=f'{t1}')
+
+        latitude = ET.SubElement(station, 'Latitude')
+        latitude.text = f'{ref_sta.latitude}'
+        longitude = ET.SubElement(station, 'Longitude')
+        longitude.text = f'{ref_sta.longitude}'
+        elevation = ET.SubElement(station, 'Elevation')
+        elevation.text = f'{ref_sta.elevation}'
+
+        ncha = ET.SubElement(station, 'TotalNumberChannels')
+        ncha.text = f'{len(inv.get_contents()["channels"])}'
+
+        for item in inv.get_contents()['channels']:
+            (net, sta, loc, cha) = item.split('.')
+            invs = inv.select(network=net, station=sta,
+                                location=loc, channel=cha)
+
+            for _cha in invs[0][0]:
+                t0 = _cha.start_date.strftime(date_fmt)
+                try:
+                        t1 = _cha.end_date.strftime(date_fmt)
+                except:
+                        t1 = None
+                channel = ET.SubElement(station, 'Channel',
+                                    code=f'{cha}',
+                                    name=f'{sta}',
+                                    locationCode=f'{loc}',
+                                    startDate=f'{t0}',
+                                    endDate=f'{t1}')
+
+                latitude = ET.SubElement(channel, 'Latitude')
+                latitude.text = f'{_cha.latitude}'
+                longitude = ET.SubElement(channel, 'Longitude')
+                longitude.text = f'{_cha.longitude}'
+                elevation = ET.SubElement(channel, 'Elevation')
+                elevation.text = f'{_cha.elevation/1e3}'
+                depth = ET.SubElement(channel, 'Depth')
+                depth.text = f'{_cha.depth}'
+                srate = ET.SubElement(channel, 'SampleRate')
+                srate.text = f'{_cha.sample_rate}'
+                
+                resp = ET.SubElement(channel, 'Response')
+                _resp = _cha.response
+                sens = ET.SubElement(resp, 'InstrumentSensitivity')
+                _sens = _resp.instrument_sensitivity
+                out_units = ET.SubElement(sens, 'OutputUnits')
+                out_units.text = _sens.output_units
+                sens_value = ET.SubElement(sens, 'Value')
+                sens_value.text = f'{_sens.value}'
+                sens_freq = ET.SubElement(sens, 'Frequency')
+                sens_freq.text = f'{_sens.frequency}'
+                in_units = ET.SubElement(sens, 'InputUnits')
+                in_units_name = ET.SubElement(in_units, 'Name')
+                in_units_name.text = _sens.input_units
+
+    tree = ET.ElementTree(root)
+    return tree
+
+def parse_utcdatetime(time):
+    """
+    Convenience function to format UTCDateTime in year-julday format
+    """
+    try:
+        val = int(time.strftime('%Y%j'))
+    except:
+        val = -1
+    return val
+
+def select_inventory(stream, inv, starttime=None, endtime=None):
+    """
+    Convenience function to select inventory from active traces only
+    """
+    invs = Inventory()
+    for tr in stream:
+        invs += inv.select(network=tr.stats.network,
+                           station=tr.stats.station,
+                           location=tr.stats.location,
+                           channel=tr.stats.channel,
+                           starttime=starttime,
+                           endtime=endtime)
+    return invs
---
+++ python/waveforms/waveform.py
@@ -0,0 +1,174 @@
+"""
+Waveform interface functions
+
+.. module:: waveform
+
+:author:
+    Jelle Assink (jelle.assink@knmi.nl)
+
+:copyright:
+    2021, Jelle Assink
+
+:license:
+    This code is distributed under the terms of the
+    GNU General Public License, Version 3
+    (https://www.gnu.org/licenses/gpl-3.0.en.html)
+"""
+from obspy import UTCDateTime, Stream
+from obspy.clients.filesystem.sds import Client as sds_client
+from obspy.clients.fdsn import Client as fdsn_client
+from obspy.clients.fdsn import RoutingClient
+import os
+
+class param_object(object):
+    pass
+
+default_celerities = (7.0, 3.0, 1.0, 0.5, 0.34, 0.3, 0.25, 0.21)
+
+def compute_celerities(t0, distance_km, celerities=None):
+    """
+    Compute timestamp for celerities
+
+    t0 - Origin time (seconds) (e.g. UNIX timestamp)
+    distance_km - Distance to epicenter (km)
+    celerities - tuple or list of celerities (km/s) to be considered
+
+    returns: dictionary with celerity labels and timestamps
+    """
+    if celerities is None:
+        celerity_keys = default_celerities
+    else:
+        celerity_keys = celerities
+    
+    celerities = dict()
+    for dist in list(distance_km):
+        for celerity in celerity_keys:
+            celerity_time_unix = (t0 + float(dist / celerity)).timestamp
+            celerities[str(celerity_time_unix)] = str(round(celerity, 2))
+    return celerities
+
+def get_data(data_source, inventory, starttime, endtime, margin_t=10):
+    """
+    source type options:
+
+    local - sds wavestructure
+    fdsn - typical dataselect
+    routing - 'iris-federator' or 'eida-routing'   
+    """
+    [(key, value)] = data_source.items()
+
+    stream = Stream()
+    for ms in stream:
+        samp_rate = ms.stats.delta
+        ms.trim(starttime, endtime-samp_rate)
+
+    if key == 'local':
+        cl = sds_client(value)
+    elif key == 'fdsn':
+        cl = fdsn_client(value)
+    elif key == 'routing':
+        cl = RoutingClient(value)
+
+    for net in inventory:
+        for sta in net:
+            for cha in sta:
+                try:
+                    st = cl.get_waveforms(network=net.code,
+                                          station=sta.code,
+                                          location=cha.location_code,
+                                          channel=cha.code,
+                                          starttime=starttime-margin_t,
+                                          endtime=endtime+margin_t)
+                    stream += st
+                except Exception as e:
+                    seed_id = f'{net.code}.{sta.code}.{cha.location_code}.{cha.code}'
+                    print(f'No data available for {seed_id}')
+                    pass
+
+    for ms in stream:
+        samp_rate = ms.stats.delta
+        ms.trim(starttime, endtime-samp_rate)
+
+    return stream
+
+def get_stream_juldays(st):
+    """
+    """
+    st.sort()
+    t0 = st[0].stats.starttime
+    t1 = st[-1].stats.endtime
+    julday_fmt = '%Y%j'
+    yj0 = int(t0.strftime(julday_fmt))
+    yj1 = int(t1.strftime(julday_fmt))
+    return range(yj0, yj1+1)
+
+def get_stream_elements(st):
+    """
+    """
+    elements = {}
+    for ms in st:
+        elements[ms.id] = ms.stats
+    return elements
+
+def stream2sds(stream, sds_root, reclen=512, wave_format='mseed', **kwargs):
+    """
+    Write ObsPy Stream out to SDS data structure
+    """
+    juldays = get_stream_juldays(stream)
+    elements = get_stream_elements(stream)
+
+    for day in juldays:
+        t0 = UTCDateTime(str(day))
+        t1 = t0 + 24*3600
+        st = stream.copy().trim(t0, t1)
+
+        for (ele, stats) in elements.items():
+            st_ele = st.select(id=ele)
+            st_ele.trim(t0, t1-stats['delta'])
+
+            if len(st_ele) > 0:
+                out_file = f'{ele}.D.{t0.year:4d}.{t0.julday:03d}'
+                out_path = (f'{sds_root}/{t0.year:4d}/'
+                            f'{stats["network"]}/'
+                            f'{stats["station"]}/'
+                            f'{stats["channel"]}.D')
+                output = f'{out_path}/{out_file}'
+
+                if os.path.isdir(out_path) is False:
+                    try:
+                        os.makedirs(out_path)
+                    except OSError as e:
+                        print (e.args)
+
+                msg = f'-> Writing {wave_format} file [ {output} ] ... '
+                print (msg, end='')
+                try:
+                    st_ele.write(output,
+                        flush=True, reclen=reclen, format=wave_format)
+                    print ('-> OK!')
+
+                except ValueError as e:
+                    print ('-> no data, not written. [ %s ]' % e)
+                    
+            else:
+                print ('-> No data for station %s on [%s - %s]' % (ele, t0, t1))
+    return
+
+def stream_count_samples(stream, **kwargs):
+    """
+    Helper function to count total number of samples in ObsPy stream
+    """
+    n_samples = 0
+    for trace in stream:
+        n_samples += trace.stats.npts
+    return n_samples
+
+def stream_unique_seed_id(stream, **kwargs):
+    """
+    Helper function to count occurrence of unique SEED ID's
+    """
+    unique_entries = []
+
+    for ms in stream:
+        unique_entries.append(ms.id)
+    return set(unique_entries)
